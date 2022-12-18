1c1a54fbb178b8b523ebf8eec648d5ef7e022704
RoxShox <19romale77@mail.ru>
2022-12-18 17:59:59 +0300

    update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {SiTelegram} from 'react-icons/si'
-import {BsInstagram} from 'react-icons/bs'
-
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { SiTelegram } from 'react-icons/si'
+import { BsInstagram } from 'react-icons/bs'
 
 const HeaderSocials = () => {
-    return (
-        <div className="header__socials">
-            <a href="https://vk.com/tranzet98" target="_blank"><BsLinkedin /></a>
-            <a href="https://github.com/tranzet23" target="_blank"><FaGithub /></a>
-            <a href="https://t.me/anzett" target="_blank"><SiTelegram /></a>
-            <a href="https://www.instagram.com/tranzet_98/" target="_blank"><BsInstagram /></a>
-        </div>
-    );
+	return (
+		<div className="header__socials">
+			<a href="https://github.com/RoxShox" target="_blank">
+				<FaGithub />
+			</a>
+			<a href="https://t.me/tlaxaL" target="_blank">
+				<SiTelegram />
+			</a>
+			<a href="" target="_blank">
+				<BsInstagram />
+			</a>
+		</div>
+	)
 }
 
-export default HeaderSocials;
+export default HeaderSocials
